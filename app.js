b698910fce1ae78d62729794c94f590e9c698972
ibrahemrass <ibrahemrass@gmail.com>
2021-01-27 15:35:10 +0200

    added a jsfile

---
+++ app.js
@@ -0,0 +1,13 @@
+//alert("hello madridesta")
+
+var real = prompt("how old are u")
+if(real > 18 ){alert("welcom")}
+else if (real < 18){alert("get out")}
+else{alert("plese answer")}
+
+var dt = new Date(); 
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+document.write(+ dt.getFullYear());
+
+
+
